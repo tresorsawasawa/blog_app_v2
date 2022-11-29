93305c36a0cefe84ffa9e251f8b394dbbf21215a
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-29 21:40:11 +0200

    Add some seeds example

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'hrttps://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+5.times do
+  Post.create(author: first_user, title: 'Hello', text: 'This is a post text')
+end    
+
+6.times do
+  Comment.create(post_id: 1, author_id: 1, text: 'Hi Tom!' )
+end
