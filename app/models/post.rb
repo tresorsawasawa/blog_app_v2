777461d40dfbc5f1470abfeca74ed8ed61adb0a7
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-29 19:04:14 +0200

    Create a method that update posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_save :updates_posts_counter
+
+  private
+
+  def  updates_posts_counter
+    posts.increament!(:posts_counter)
+  end
 end
